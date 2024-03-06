de0da68ba1e0e7dc42ea58721362a0564670deaa
A00431008 <Sudeep.Raj.Badal@smu.ca>
2024-03-06 00:04:35 -0400

    Initializes Springboot Project

---
+++ src/main/java/com/example/springboothotels/SpringbootHotelsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboothotels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHotelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootHotelsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboothotels/SpringbootHotelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboothotels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootHotelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
