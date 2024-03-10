5f3bcd285d12268c51859bb25a8518acddf1cb48
A00431008 <Sudeep.Raj.Badal@smu.ca>
2024-03-09 21:33:27 -0400

    Improved post method to check if hotel already exists

--- src/main/java/com/example/springboothotels/controllers/hotelsController.java
+++ src/main/java/com/example/springboothotels/controllers/hotelsController.java
@@ -1,14 +1,11 @@
 package com.example.springboothotels.controllers;
-
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.TypedQuery;
 import jakarta.transaction.Transactional;
-//import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import com.example.springboothotels.models.Hotel;
 import java.util.List;
 
@@ -24,10 +21,50 @@ public class hotelsController {
         return query.getResultList();
     }
 
+    private boolean hotelExists(Hotel hotel) {
+        TypedQuery<Hotel> query = entityManager.createQuery("SELECT h from Hotel h ", Hotel.class);
+        List<Hotel> existingHotel = query.getResultList();
+        for (Hotel hotelFromList : existingHotel) {
+            if (hotel.getName().equals(hotelFromList.getName())) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     @Transactional
     @PostMapping(value = "/addHotel", consumes = "application/json")
-    public Hotel addHotel(@RequestBody Hotel hotel) {
-        entityManager.persist(hotel);
-        return hotel;
+    public ResponseEntity<String> addHotel(@RequestBody Hotel hotel) {
+        try {
+            if (hotelExists(hotel)) {
+                String response = "Hotel with name \"" + hotel.getName() 
+                    + "\" Already Exists !! Cannot add same hotel twice !!!";
+                return new ResponseEntity<>(response, HttpStatus.CONFLICT);
+            } else {
+                entityManager.persist(hotel);
+                String response = "Hotel Added Successfully !!!";
+                return new ResponseEntity<>(response, HttpStatus.OK);
+            }
+        } catch (Exception e) {
+            return new ResponseEntity<>("Error: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
+
+
+    // Added Delete method to test
+    @Transactional
+    @DeleteMapping(value = "/deleteHotel/{id}")
+    public ResponseEntity<String> deleteHotel(@PathVariable Long id) {
+        Hotel hotelToDelete = entityManager.find(Hotel.class, id);
+
+        if (hotelToDelete == null) {
+            return new ResponseEntity<>("Hotel not found with ID: " + id, HttpStatus.NOT_FOUND);
+        }
+
+        entityManager.remove(hotelToDelete);
+
+        return new ResponseEntity<>("Hotel deleted successfully", HttpStatus.OK);
+    }
+
 }
